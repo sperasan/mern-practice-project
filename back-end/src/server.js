6a898761b574592863745e046f455363e55396c9
sean-golark <56940505+sperasan@users.noreply.github.com>
2026-05-20 06:32:28 +0800

    hooking up the backend to mongodb

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import { MongoClient, ReturnDocument, ServerApiVersion } from "mongodb";
 import admin from "firebase-admin";
 import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __directory = path.dirname(__filename);
 
 const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
 admin.initializeApp({
@@ -10,7 +15,9 @@ admin.initializeApp({
 
 let db;
 const connectToDb = async () => {
-  const uri = "mongodb://127.0.0.1:27017";
+  const uri = !process.env.MONGODB_USERNAME
+    ? "mongodb://127.0.0.1:27017"
+    : `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.g60mzjt.mongodb.net/?appName=Cluster0`;
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -25,6 +32,12 @@ const connectToDb = async () => {
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__directory, "../dist")));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__directory, "../dist/index.html"));
+});
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
@@ -85,10 +98,12 @@ app.post("/api/articles/:name/comment", async (req, res) => {
   res.json(article);
 });
 
+const PORT = process.env.PORT || 8000;
+
 const start = async () => {
   await connectToDb();
-  app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
   });
 };
 
